9bf949d766eeb07dbae592c8d65dde1135ea95b2
arthurdmc2005 <arthurdiasmoreiracorrea@gmail.com>
2026-05-19 15:00:52 -0300

    bugfix:  Beans do redis tentavam conectar em um runner inexistente

--- src/test/java/com/LigaAcademic/AcademicProject/AcademicProjectApplicationTests.java
+++ src/test/java/com/LigaAcademic/AcademicProject/AcademicProjectApplicationTests.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import com.LigaAcademic.AcademicProject.Infra.seeder.AdminSeeder;
+import io.lettuce.core.RedisClient;
+import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -29,6 +31,14 @@ class AcademicProjectApplicationTests {
 
     @MockBean
     private AdminSeeder adminSeeder;
+
+    @MockBean
+    private RedisClient redisClient;
+
+    @MockBean
+    @SuppressWarnings("rawtypes")
+    private LettuceBasedProxyManager lettuceBasedProxyManager;
+
 
     @Test
     void contextLoads() {
