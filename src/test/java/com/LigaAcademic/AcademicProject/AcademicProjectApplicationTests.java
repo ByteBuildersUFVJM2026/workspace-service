432c38e4c6ea8feac8a0c775bf9449f574c771d6
arthurdmc2005 <arthurdiasmoreiracorrea@gmail.com>
2026-05-19 14:47:33 -0300

    Corrigindo problemas na V7 que impossibilitavam os testes

--- src/test/java/com/LigaAcademic/AcademicProject/AcademicProjectApplicationTests.java
+++ src/test/java/com/LigaAcademic/AcademicProject/AcademicProjectApplicationTests.java
@@ -2,12 +2,35 @@ package com.LigaAcademic.AcademicProject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import com.LigaAcademic.AcademicProject.Infra.seeder.AdminSeeder;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Testcontainers
 class AcademicProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
 
+    @MockBean
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @MockBean
+    private ReactiveRedisConnectionFactory reactiveRedisConnectionFactory;
+
+    @MockBean
+    private AdminSeeder adminSeeder;
+
+    @Test
+    void contextLoads() {
+    }
 }
